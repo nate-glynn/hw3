c870f42322c4ff79701b4ab12f3f9643946e28f5
Nathan Glynn <nathan.glynn@kellogg.northwestern.edu>
2025-02-22 15:34:36 +0000

    initial controllers built

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,9 @@
+class EntriesController < ApplicationController
+
+  def index
+
+    render :inline => "Test 1"
+    
+  end
+
+end
---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,8 @@
+class PlacesController < ApplicationController
+
+  def index
+
+    render :inline => "Test"
+    
+  end
+end
